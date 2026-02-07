a4525fba496408ce6b06b3adc48a415c83286f56
smarek830 <starymarek@gmail.com>
2026-02-07 23:39:10 +0100

    Add new URL paths for machine management and web interfaces

--- core/urls.py
+++ core/urls.py
@@ -9,25 +9,25 @@ urlpatterns = [
     path('plan/export/', views.export_plan_excel, name='export_plan_excel'),
     path('zakazka/<int:pk>/', views.detail_zakazky, name='detail_zakazky'),
     path('stroje/', views.zoznam_strojov, name='zoznam_strojov'),
-    
+
     # WEB FORMULÁRE - Nové objednávky a kontrakty
     path('objednavka/nova/', views.nova_objednavka, name='nova_objednavka'),
     path('objednavka/<int:pk>/upravit/', views.upravit_objednavku, name='upravit_objednavku'),
     path('kontrakt/novy/', views.novy_kontrakt, name='novy_kontrakt'),
     path('kontrakt/<int:pk>/upravit/', views.upravit_kontrakt, name='upravit_kontrakt'),
-    
+
     # SKLADY - NOVÉ
     path('sklad/', views.sklad_hotovych_dielov, name='sklad_hotovych_dielov'),
     path('sklad/material/', views.sklad_materialu, name='sklad_materialu'),
-    
+
     # Výrobné dávky
     path('kontrakt/<int:kontrakt_pk>/vytvor-davku/', views.vytvor_davku_z_kontraktu, name='vytvor_davku_z_kontraktu'),
     path('davka/<int:davka_pk>/vytvor-objednavku/', views.vytvor_objednavku_z_davky, name='vytvor_objednavku_z_davky'),
-    
+
     # Operátorské URL
     path('operator/', views.operator_dashboard, name='operator_dashboard'),
     path('operator/zakazka/<int:pk>/', views.operator_zakazka_detail, name='operator_zakazka_detail'),
-    
+
     # AJAX API
     path('api/operator/start/<int:objednavka_pk>/<int:operacia_pk>/', views.start_operation, name='start_operation'),
     path('api/operator/pause/<int:objednavka_pk>/<int:operacia_pk>/', views.pause_operation, name='pause_operation'),
@@ -35,7 +35,7 @@ urlpatterns = [
     path('api/operator/end-work/<int:pk>/', views.end_work, name='end_work'),
     path('api/operator/report-problem/<int:pk>/', views.report_problem, name='report_problem'),
     path('api/sprievodka/<int:pk>/', views.download_sprievodka, name='download_sprievodka'),
-]
+
     # Nové webové rozhrania
     path('stroj/novy/', views.novy_stroj, name='novy_stroj'),
     path('stroj/<int:pk>/upravit/', views.upravit_stroj, name='upravit_stroj'),
