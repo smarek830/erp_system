35a1b7b4249741ce1487ca5d1ea03500a2cc2e2c
smarek830 <starymarek@gmail.com>
2026-02-23 16:38:26 +0100

    Allow Tailscale host

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-w^xswd6i#%kjz46y5chs6nnbpcth52j=))&x-#4x=-t3om)&gp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.1.94', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.1.94', '100.88.66.23', 'localhost', '127.0.0.1']
 
 
 # Application definition
