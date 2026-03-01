b4ddb1975c9352a3ec7d6f4bb94824ac0e64fd32
smarek830 <starymarek@gmail.com>
2026-03-01 22:00:30 +0100

    Serve static files in DEBUG via staticfiles_urlpatterns

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include  # <--- Pridaj 'include'
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.urls import path, include
 from core import views as core_views
@@ -19,4 +20,5 @@ urlpatterns = [
 
 # Toto zabezpečí, že budú fungovať odkazy na PDF a obrázky
 if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
